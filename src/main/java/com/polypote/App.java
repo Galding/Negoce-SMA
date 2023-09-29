a3453a5902daf4a460379e7602fe06d02efce798
Galding <nathan.peyronnet@gmail.com>
2023-09-29 14:44:17 +0200

    initial commit

---
+++ src/main/java/com/polypote/App.java
@@ -0,0 +1,13 @@
+package com.polypote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
